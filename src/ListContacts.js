b2e13c70c0baeab22dc4a8e64a07a2c5cb5a1371
Ismail Omodara <omodara145@gmail.com>
2020-12-02 08:11:08 +0100

    State management

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,20 +1,57 @@
-import React  from 'react'
+import React, { Component }  from 'react'
+import PropTypes from 'prop-types'
 
-const ListContacts = (props) => {
-  return (
-      <ol className='contact-list'>
-        {props.contacts.map(contact => <li key={contact.id} className='contact-list-item'>
-          <div className='contact-avatar' style={{ backgroundImage: `url(${contact.avatarURL}`}}></div>
-          <div className='contact-details'>
-            <p>{contact.name}</p>
-            <p>@{contact.handle}</p>
+class ListContacts extends Component {
+  static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onContactRemove: PropTypes.func.isRequired
+  }
+  state = {
+    query: ''
+  }
+  updateQuery = (query) => {
+    this.setState(() => ({ query: query.trim() }))
+  }
+  resetQuery = () => {
+    this.updateQuery('')
+  }
+  render() {
+    const { query } = this.state
+    const { contacts, onContactRemove } = this.props
+
+    const filteredContacts = query === '' ?
+        contacts : contacts.filter(contact => contact.name.toLowerCase().includes(query.toLowerCase()))
+    return (
+        <div className='list-contacts'>
+          <div className='list-contacts-top'>
+            <input
+                className='search-contacts'
+                type='text'
+                placeholder='Search contacts'
+                value={query}
+                onChange={(event) => this.updateQuery(event.target.value)} />
           </div>
-          <button onClick={() => props.onContactRemove(contact)} className='contact-remove'>
-            Remove
-          </button>
-        </li>)}
-      </ol>
-  )
+          {filteredContacts.length !== contacts.length && (
+              <div className='showing-contacts'>
+                <p>Now showing {filteredContacts.length} of {contacts.length}.</p>
+                <button onClick={this.resetQuery}>Show all</button>
+              </div>
+          )}
+          <ol className='contact-list'>
+            {filteredContacts.map(contact => <li key={contact.id} className='contact-list-item'>
+              <div className='contact-avatar' style={{ backgroundImage: `url(${contact.avatarURL}`}}></div>
+              <div className='contact-details'>
+                <p>{contact.name}</p>
+                <p>@{contact.handle}</p>
+              </div>
+              <button onClick={() => onContactRemove(contact)} className='contact-remove'>
+                Remove
+              </button>
+            </li>)}
+          </ol>
+        </div>
+    )
+  }
 }
 
 export default ListContacts
